4c5ad9fdaf72b50d4e1bd2b293f8c3f95436f100
ChiaChing-Yen <ccyen@umich.edu>
2025-02-18 13:04:54 +0800

    refactor: remove unused timezone function from streetview_simple.py

--- google_maps_api/streetview_simple.py
+++ google_maps_api/streetview_simple.py
@@ -12,40 +12,6 @@ TIMEZONE_BASE_URL = "https://maps.googleapis.com/maps/api/timezone/json"
 STREETVIEW_BASE_URL = "https://maps.googleapis.com/maps/api/streetview"
 PWD = pathlib.Path(__file__).parent.absolute()
 
-def timezone(lat, lng, timestamp):
-    """# Build the parameters dictionary"""
-    params = {
-        "location": f"{lat},{lng}",
-        "timestamp": timestamp,
-        "key": API_KEY
-    }
-
-    current_delay = 0.1  # Initial retry delay of 100ms
-    max_delay = 5  # Maximum retry delay of 5 seconds
-
-    while True:
-        try:
-            # Make the GET request using requests
-            response = requests.get(TIMEZONE_BASE_URL, params=params)
-            response.raise_for_status()  # Raises an HTTPError for bad responses
-            
-            result = response.json()
-
-            if result["status"] == "OK":
-                return result["timeZoneId"]
-            elif result["status"] != "UNKNOWN_ERROR":
-                # Many API errors cannot be fixed by a retry, e.g. INVALID_REQUEST or
-                # ZERO_RESULTS. There is no point retrying these requests.
-                raise Exception(result["error_message"])
-
-        except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):
-            if current_delay > max_delay:
-                raise Exception("Too many retry attempts.")
-
-            print("Waiting", current_delay, "seconds before retrying.")
-            time.sleep(current_delay)
-            current_delay *= 2  # Increase the delay each time we retry
-
 
 def streetview(lat, lng, heading, pitch, fov=90, size_x=640, size_y=640):
     """
